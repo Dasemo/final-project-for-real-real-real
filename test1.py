c2f8c88d030cd0f3dc60d4db77249d20656e9f64
marcos3211 <marcoxroblex@gmail.com>
2023-12-08 17:34:35 +0100

    a

--- test1.py
+++ test1.py
@@ -1,48 +1,187 @@
 import pyxel
 
-class Game:
-    def __init__(self):
-        pyxel.init(160, 120)
-        self.player_x = 75
-        self.player_y = 100
+#pyxel edit assets/ey.pyxres
 
-        self.enemy_x = 30
-        self.enemy_y = 100
+class Properties:
+    def __init__(self, x, y, u, v, w, h, sprite=0):
+        self.__x = x
+        self.__y = y
+        self.__u = u
+        self.__v = v
+        self.__w = w
+        self.__h = h
+        self.sprite = sprite
 
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @property
+    def u(self):
+        return self.__u
+
+    @property
+    def v(self):
+        return self.__v
+
+    @property
+    def w(self):
+        return self.__w
+
+    @property
+    def h(self):
+        return self.__h
+
+    @x.setter
+    def x(self, x):
+        if x < 0:
+            self.__x = 0
+        else:
+            self.__x = x
+
+    @y.setter
+    def y(self, y):
+        if y < 0:
+            self.__y = 0
+        else:
+            self.__y = y
+
+    @u.setter
+    def u(self, u):
+        if type(u) is not int:
+            raise TypeError("u must be an integer")
+        elif u > pyxel.width:
+            self.__u = pyxel.width
+        elif u < 0:
+            self.__u = 0
+        else:
+            self.__u = u
+
+    @v.setter
+    def v(self, v):
+        if type(v) is not int:
+            raise TypeError("v must be an integer")
+        elif v > pyxel.height:
+            self.__v = pyxel.height
+        elif v < 0:
+            self.__v = 0
+        else:
+            self.__v = v
+
+    @w.setter
+    def w(self, w):
+        if type(w) is not int:
+            raise TypeError("w must be an integer")
+        elif w > pyxel.width:
+            self.__w = pyxel.width
+        elif w < -pyxel.width:
+            self.__w = -w
+        else:
+            self.__w = w
+
+    @h.setter
+    def h(self, h):
+        if type(h) is not int:
+            raise TypeError("h must be an integer")
+        elif h > pyxel.height:
+            self.__h = pyxel.height
+        elif h < -pyxel.height:
+            self.__h = -h
+        else:
+            self.__h = h
+
+class Player(Properties):
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.sprite = (0, 0, 0, 16, 22)
+        self.lives = 3
+        self.vel_y = 0
+        self.jumping = False
+
+    def move(self, direction: str, size: int):
+        xSize = self.sprite[3]
+        if direction.lower() == 'right' and self.x < size - xSize:
+            self.x = self.x + 4
+        elif direction.lower() == 'left' and self.x > 0:
+            self.x -= 4
+
+    def jump(self):
+        if not self.jumping:
+            self.vel_y = -8
+            self.jumping = True
+
+
+    def update(self):
+        if not self.ground():
+            self.vel_y += 0.5
+
+        self.y += self.vel_y
+
+        # Ajusta la lógica de colisión para que Mario no se hunda por debajo del suelo
+        if self.ground():
+            self.y = min(self.y, self.groundHeight())
+            self.vel_y = 0
+            self.jumping = False
+        else:
+            self.jumping = True  # Permite que Mario siga saltando mientras esté en el aire
+
+
+    def ground(self):
+        return self.y == self.groundHeight()
+
+    def groundHeight(self):
+        return 218
+
+
+class App:
+    def __init__(self, w: int, h: int):
+        self.width = w
+        self.height = h
+
+        pyxel.init(self.width, self.height, title="Mario Bros. Classic")
+        pyxel.load("assets/ey.pyxres")
+
+        self.plane = Player(self.width // 2, 218)
+
+        self.drawing = [
+            [0, 240, 0, 48, 104, 16, 16], # Bloque de brick
+            [0, 200, 0, 64, 104, 8, 8] # Bloque azul
+        ] 
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        # Mover el jugador
-        if pyxel.btn(pyxel.KEY_LEFT) and self.player_x > 0:
-            self.player_x -= 1
-        if pyxel.btn(pyxel.KEY_RIGHT) and self.player_x < pyxel.width - 10:
-            self.player_x += 1
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+        elif pyxel.btnp(pyxel.KEY_SPACE):
+            self.plane.jump()
+        elif pyxel.btn(pyxel.KEY_RIGHT):
+            self.plane.move('right', self.width)
+        elif pyxel.btn(pyxel.KEY_LEFT):
+            self.plane.move('left', self.width)
 
-        # Mover el enemigo (en este caso, el enemigo se mueve automáticamente)
-        self.enemy_x += 2
-        if self.enemy_x > pyxel.width:
-            self.enemy_x = 0
-
-        # Detección de colisión
-        player_rect = (self.player_x, self.player_y, 10, 10)
-        enemy_rect = (self.enemy_x, self.enemy_y, 10, 10)
-
-        if self.check_collision(player_rect, enemy_rect):
-            print("¡Colisión!")
+        # Update the plane's state
+        self.plane.update()
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.player_x, self.player_y, 10, 10, 9)  # Dibuja al jugador
-        pyxel.rect(self.enemy_x, self.enemy_y, 10, 10, 8)  # Dibuja al enemigo
 
-    def check_collision(self, rect1, rect2):
-        # Verifica si dos rectángulos se intersectan
-        return (
-            rect1[0] < rect2[0] + rect2[2] and
-            rect1[0] + rect1[2] > rect2[0] and
-            rect1[1] < rect2[1] + rect2[3] and
-            rect1[1] + rect1[3] > rect2[1]
-        )
+        # Dibujar el mapa
+        # Primer valor, x de la pantalla. Segundo la y. Tercero es 0 siempre. Cuarto es de la imagen cuanto a la derecha.
+        #Quinto altura de la imagen. Sexto es ancho de la imagen. Ultimo es cuanto coge de arriba
+        for i in range(len(self.drawing)):
+            a = 0
+            for e in range(33):
+                pyxel.blt(a, self.drawing[0][1], self.drawing[0][2], self.drawing[0][3], self.drawing[0][4], self.drawing[0][5], self.drawing[0][6])
+                a = a + 8
+            pyxel.blt(self.drawing[1][0], self.drawing[1][1], self.drawing[1][2], self.drawing[1][3], self.drawing[1][4], self.drawing[1][5], self.drawing[1][6])
+            
+        # Dibujar el jugador
+        pyxel.blt(self.plane.x, self.plane.y, 0, 0, 10, 16, 22)
 
-if __name__ == "__main__":
-    Game()
+# Ejecutar la aplicación
+app = App(255, 255)
