ab9647845396702cafef7593185356f969ccee90
Dasemo <100522244@alumnos.uc3m.es>
2023-12-10 20:46:06 +0100

    no se como hacer una mierda de los enemigos pero yo lo sigo intentando xd

--- enemies.py
+++ enemies.py
@@ -12,8 +12,25 @@ class Shellcreeper(Properties):
         self.is_mad = False
     
     def update(self):
-        self.dx = self.direction
         self.x += 1
-        if self.direction < 0:
-            self.x = 255
+        if self.x >= 255 or self.x < 0:
+            self.x = (self.x + 255) % 255
+            self.direction *= -1
+
+
+class Sidestepper(Properties):
+    def __init(self, x, y, is_falling):
+        self.x = x
+        self.y = y
+        self.dx = 0
+        self.dy = 0
+        self.direction = -1
+        self.is_falling = False
+        self.is_alive = True
+        self.is_mad = False
     
+    def update(self):
+        self.x += 1
+        if self.x >= 255 or self.x < 0:
+            self.x = (self.x + 255) % 255
+            self.direction *= -1
