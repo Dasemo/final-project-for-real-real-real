c7fe903ccac301de309daba03853ef5b1dc5e9c0
marcos3211 <marcoxroblex@gmail.com>
2023-12-11 18:00:00 +0100

    a

--- test1.py
+++ test1.py
@@ -1,5 +1,4 @@
 import pyxel
-from map import Map
 
 class Properties:
     def __init__(self, x, y, u, v, w, h, sprite=0):
@@ -49,50 +48,6 @@ class Properties:
         else:
             self.__y = y
 
-    @u.setter
-    def u(self, u):
-        if type(u) is not int:
-            raise TypeError("u must be an integer")
-        elif u > pyxel.width:
-            self.__u = pyxel.width
-        elif u < 0:
-            self.__u = 0
-        else:
-            self.__u = u
-
-    @v.setter
-    def v(self, v):
-        if type(v) is not int:
-            raise TypeError("v must be an integer")
-        elif v > pyxel.height:
-            self.__v = pyxel.height
-        elif v < 0:
-            self.__v = 0
-        else:
-            self.__v = v
-
-    @w.setter
-    def w(self, w):
-        if type(w) is not int:
-            raise TypeError("w must be an integer")
-        elif w > pyxel.width:
-            self.__w = pyxel.width
-        elif w < -pyxel.width:
-            self.__w = -w
-        else:
-            self.__w = w
-
-    @h.setter
-    def h(self, h):
-        if type(h) is not int:
-            raise TypeError("h must be an integer")
-        elif h > pyxel.height:
-            self.__h = pyxel.height
-        elif h < -pyxel.height:
-            self.__h = -h
-        else:
-            self.__h = h
-
 class Player(Properties):
     def __init__(self, x: int, y: int):
         super().__init__(x, y, 0, 0, 16, 22)
@@ -107,7 +62,7 @@ class Player(Properties):
         elif direction.lower() == 'left' and self.x > 0:
             self.x -= 4
         elif self.x == 0 and direction.lower() == 'left':
-            self.x = size - xSize
+            self.x = 255
         elif self.x == size - xSize and direction.lower() == 'right':
             self.x = 0
 
@@ -122,13 +77,13 @@ class Player(Properties):
 
         self.y += self.vel_y
 
-        # Adjust the collision logic to prevent Mario from sinking below the ground
+        # Adjust collision logic to prevent Mario from sinking below the ground
         if self.ground():
             self.y = min(self.y, self.groundHeight())
             self.vel_y = 0
             self.jumping = False
         else:
-            self.jumping = True  # Allows Mario to keep jumping while in the air
+            self.jumping = True
 
     def ground(self):
         return self.y == self.groundHeight()
@@ -144,19 +99,104 @@ class Enemy(Properties):
     def move(self):
         self.x += self.speed
         if self.x > pyxel.width:
-            self.x = -16  # Reset the position when the enemy goes off-screen
+            self.x = -16
+
+def check_collision(entity1, entity2):
+    """
+    Check for collision between two rectangular entities.
+
+    Parameters:
+    - entity1: An object representing the first entity with properties 'x', 'y', 'w' (width), and 'h' (height).
+    - entity2: An object representing the second entity with properties 'x', 'y', 'w' (width), and 'h' (height).
+
+    Returns:
+    - True if collision is detected, False otherwise.
+    """
+    left1, right1, top1, bottom1 = entity1.x, entity1.x + entity1.w, entity1.y, entity1.y + entity1.h
+    left2, right2, top2, bottom2 = entity2.x, entity2.x + entity2.w, entity2.y, entity2.y + entity2.h
+
+    # Check for overlap along both axes
+    return not (right1 < left2 or left1 > right2 or bottom1 < top2 or top1 > bottom2)
+
+class Map:
+    def __init__(self):
+        self.drawing = [
+            [0, 240, 0, 48, 104, 16, 16],  # Brick block
+            [0, 0, 0, 64, 104, 8, 8],  # Blue block
+            [0, 0, 0, 80, 104, 16, 16],  # Straight pipe
+            [0, 0, 0, 0, 104, 16, 16],  # POW
+            [0, 0, 0, 128, 104, 16, 16],  # Pipe2
+            [0, 0, 0, 110, 104, 16, 16],  # Pipe3
+        ]
+
+    def draw(self):
+        for i in range(len(self.drawing)):
+            a = 0
+            for e in range(33):
+                pyxel.blt(a, self.drawing[0][1], self.drawing[0][2], self.drawing[0][3], self.drawing[0][4], self.drawing[0][5], self.drawing[0][6])  # ground
+                a = a + 8
+            a = 0
+            for xd in range(11):
+                pyxel.blt(a, self.drawing[1][1] + 184, self.drawing[1][2], self.drawing[1][3], self.drawing[1][4], self.drawing[1][5], self.drawing[1][6])  # first level left
+                a = a + 8
+            a = 255
+            for xd in range(12):
+                pyxel.blt(a, self.drawing[1][1] + 184, self.drawing[1][2], self.drawing[1][3], self.drawing[1][4], self.drawing[1][5], self.drawing[1][6])  # first level right
+                a = a - 8
+            pyxel.blt(239, 223, self.drawing[2][2], self.drawing[2][3], self.drawing[2][4], self.drawing[2][5], self.drawing[2][6])  # Pipe right
+            pyxel.blt(0, 223, self.drawing[2][2], self.drawing[2][3], self.drawing[2][4], -self.drawing[2][5], self.drawing[2][6])  # Pipe left
+            pyxel.blt(120, 184, self.drawing[3][2], self.drawing[3][3], self.drawing[3][4], self.drawing[3][5], self.drawing[3][6])  # POW
+            a = 0
+            for xd in range(4):
+                pyxel.blt(a, self.drawing[1][1] + 136, self.drawing[1][2], self.drawing[1][3], self.drawing[1][4], self.drawing[1][5], self.drawing[1][6])  # first level left
+                a = a + 8
+            a = 255
+            for xd in range(5):
+                pyxel.blt(a, self.drawing[1][1] + 136, self.drawing[1][2], self.drawing[1][3], self.drawing[1][4], self.drawing[1][5], self.drawing[1][6])  # first level left
+                a = a - 8
+            a = 0
+            for xd in range(11):
+                pyxel.blt(a, self.drawing[1][1] + 72, self.drawing[1][2], self.drawing[1][3], self.drawing[1][4], self.drawing[1][5], self.drawing[1][6])  # first level left
+                a = a + 8
+            a = 255
+            for xd in range(12):
+                pyxel.blt(a, self.drawing[1][1] + 72, self.drawing[1][2], self.drawing[1][3], self.drawing[1][4], self.drawing[1][5], self.drawing[1][6])  # first level left
+                a = a - 8
+            a = 64
+            for xd in range(16):
+                pyxel.blt(a, self.drawing[1][1] + 120, self.drawing[1][2], self.drawing[1][3], self.drawing[1][4], self.drawing[1][5], self.drawing[1][6])  # first level left
+                a = a + 8
+
+            pyxel.blt(self.drawing[4][0] + 16, self.drawing[4][1] + 48, self.drawing[4][2], self.drawing[4][3], self.drawing[4][4], -self.drawing[4][5], self.drawing[4][6])  # Pipe2 left
+            pyxel.blt(self.drawing[2][0], self.drawing[2][1] + 47, self.drawing[2][2], self.drawing[2][3], self.drawing[2][4], -self.drawing[2][5], self.drawing[2][6])  # Pipe1 left
+            pyxel.blt(self.drawing[5][0] + 18, self.drawing[5][1] + 32, self.drawing[5][2], self.drawing[5][3], self.drawing[5][4], -self.drawing[5][5], self.drawing[5][6])  # Pipe3 left
+            pyxel.blt(self.drawing[2][0] + 34, self.drawing[2][1] + 32, self.drawing[2][2], self.drawing[2][3], self.drawing[2][4], -self.drawing[2][5], self.drawing[2][6])  # Pipe1(2) left
+
+            pyxel.blt(self.drawing[4][0] + 255 - 32, self.drawing[4][1] + 48, self.drawing[4][2], self.drawing[4][3], self.drawing[4][4], self.drawing[4][5], self.drawing[4][6])  # Pipe2 right
+            pyxel.blt(self.drawing[5][0] + 255 - 34, self.drawing[5][1] + 32, self.drawing[5][2], self.drawing[5][3], self.drawing[5][4], self.drawing[5][5], self.drawing[5][6])  # Pipe3 right
+            pyxel.blt(self.drawing[2][0] + 255 - 50, self.drawing[2][1] + 32, self.drawing[2][2], self.drawing[2][3], self.drawing[2][4], self.drawing[2][5], self.drawing[2][6])  # Pipe1(2) right
+            pyxel.blt(self.drawing[2][0] + 239, self.drawing[2][1] + 47, self.drawing[2][2], self.drawing[2][3], self.drawing[2][4], self.drawing[2][5], self.drawing[2][6])  # Pipe1 right
 
 class App:
-    def __init__(self, w: int, h: int):
-        self.width = w
-        self.height = h
+    _initialized = False
 
-        pyxel.init(self.width, self.height, title="Mario Bros. Classic")
-        pyxel.load("assets/ey.pyxres")
+    def __init__(self):
+        self.width = 255
+        self.height = 255
 
+        # Initialize Pyxel only if it hasn't been initialized before
+        if not App._initialized:
+            pyxel.init(self.width, self.height, title="Mario Bros. Classic")
+            pyxel.load("assets/ey.pyxres")
+            App._initialized = True
+
+        # Create an instance of the Player class and assign it to the 'plane' attribute
         self.plane = Player(self.width // 2, 218)
         self.enemy = Enemy(20, 218, speed=1)
         self.mapa = Map()
+
+    # ... (rest of the App class)
+
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
@@ -172,27 +212,32 @@ class App:
         self.plane.update()
         # Update the enemy's state
         self.enemy.move()
-        if self.check_collision(self.plane, self.enemy):
+
+        # Check for collision with enemy
+        if check_collision(self.plane, self.enemy):
             print("Collision! Mario touched the enemy.")
 
-    def check_collision(self, obj1, obj2):
-        return (
-            obj1.x < obj2.x + obj2.w and
-            obj1.x + obj1.w > obj2.x and
-            obj1.y < obj2.y + obj2.h and
-            obj1.y + obj1.h > obj2.y
-        )
+        # Check for collision with each element in the map
+        for element in self.mapa.drawing:
+            element_properties = {
+                "x": element[0],
+                "y": element[1],
+                "w": element[4],
+                "h": element[5]
+            }
+            if check_collision(self.plane, element_properties):
+                print(f"Collision! Mario touched an element at ({element[0]}, {element[1]})")
+
+    # ... (remaining code)
+
 
     def draw(self):
         pyxel.cls(0)
         self.mapa.draw()
 
-        # Draw the enemy
-        pyxel.blt(self.enemy.x, self.enemy.y, 0, 0, 10, 16, 22, 0)
-
         # Draw the player
-        pyxel.blt(self.plane.x, self.plane.y, 0, 0, 10, 16, 22, 0)
+        pyxel.blt(self.plane.x, self.plane.y, 0, 0, 10, 16, 22)
 
 # Run the application
-app = App(255, 255)
+app = App()
 pyxel.run(app.update, app.draw)
