0bae1e3eb72bd4e91df98df39a0e3dcbcfbdc618
Dasemo <100522244@alumnos.uc3m.es>
2023-12-11 17:32:05 +0100

    cosa que se mueve de lao a lao

--- enemies.py
+++ enemies.py
@@ -1,25 +1,29 @@
+import pyxel
 from properties import Properties
 
 class Shellcreeper(Properties):
-    def __init__(self, x, y, is_falling):
+    def __init__(self, x, y, speed):
         self.x = x
         self.y = y
-        self.dx = 0
-        self.dy = 0
-        self.direction = -1
-        self.is_falling = False
-        self.is_alive = True
-        self.is_mad = False
-    
+        self.speed = speed
+        self.direction = 1  # 1 for right, -1 for left
+        self.width = 16
+        self.height = 16
+
     def update(self):
-        self.x += 1
-        if self.x >= 255 or self.x < 0:
-            self.x = (self.x + 255) % 255
+        # Move sidestepper
+        self.x += self.speed * self.direction
+
+        # Check for collisions with walls and change direction
+        if self.x < 0 or self.x + self.width > pyxel.width:
             self.direction *= -1
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, 2, 2, self.width * self.direction, self.height, 0)
 
 
 class Sidestepper(Properties):
-    def __init(self, x, y, is_falling):
+    def __init__(self, x, y, is_falling):
         self.x = x
         self.y = y
         self.dx = 0
