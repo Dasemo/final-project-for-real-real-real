d3112263d3301d1515b46f407b0acc2b5c6a3a33
Dasemo <100522244@alumnos.uc3m.es>
2023-12-11 17:58:28 +0100

    ayuda por favor no puedo mas con mi vida

--- enemies.py
+++ enemies.py
@@ -14,27 +14,32 @@ class Shellcreeper(Properties):
         # Move sidestepper
         self.x += self.speed * self.direction
 
-        # Check for collisions with walls and change direction
-        if self.x < 0 or self.x + self.width > pyxel.width:
-            self.direction *= -1
+        if self.x > 0:
+            self.x == self.x + self.width > pyxel.width
+        if self.x == self.x + self.width > pyxel.width:
+            self.x = 0
 
     def draw(self):
-        pyxel.blt(self.x, self.y, 0, 2, 2, self.width * self.direction, self.height, 0)
+        pyxel.blt(self.x, self.y, 0, 1, 32, self.width * self.direction, self.height, 0)
 
 
 class Sidestepper(Properties):
-    def __init__(self, x, y, is_falling):
+    def __init__(self, x, y, speed):
         self.x = x
         self.y = y
-        self.dx = 0
-        self.dy = 0
-        self.direction = -1
-        self.is_falling = False
-        self.is_alive = True
-        self.is_mad = False
-    
+        self.speed = speed
+        self.direction = 1  # 1 for right, -1 for left
+        self.width = 16
+        self.height = 16
+
     def update(self):
-        self.x += 1
-        if self.x >= 255 or self.x < 0:
-            self.x = (self.x + 255) % 255
-            self.direction *= -1
+        # Move sidestepper
+        self.x += self.speed * self.direction
+
+        if self.x > 0:
+            self.x == self.x + self.width > pyxel.width
+        if self.x == self.x + self.width > pyxel.width:
+            self.x = 0
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, 1, 32, self.width * self.direction, self.height, 0)
