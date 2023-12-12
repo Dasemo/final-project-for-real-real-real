fdd648a4ec7d4a6168e01f4bd7fb77ca08e0ad9b
Dasemo <100522244@alumnos.uc3m.es>
2023-12-12 01:55:02 +0100

    ultima modificacion con ganas de cortarme las venas y hasta de su-

--- app.py
+++ app.py
@@ -50,4 +50,7 @@ class App:
         self.fighter.draw()
         # Dibujar el jugador
         pyxel.blt(self.plane.x, self.plane.y, 0, 0, 10, 16, 22)
+        pyxel.blt(60, 20, 0, 8, 0, 8, 8, 0)
+        pyxel.blt(70, 20, 0, 8, 0, 8, 8, 0)
+        pyxel.blt(80, 20, 0, 8, 0, 8, 8, 0)
        
